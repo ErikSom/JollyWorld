2981ec003aa25d400ecdc00d7e57ad70d900144d
Erik Sombroek <eriksombroek@gmail.com>
2017-12-26 08:12:36 +0100

    Added full unique username selector in firebase

--- src/FireBaseManager.js
+++ src/FireBaseManager.js
@@ -1,76 +1,98 @@
 function FireBaseManager() {
     this.app;
+    this.user;
+    this.username;
     this.usernameUnique = false;
-    this.doRegister = false;
+    this.createdUserdata = false;
     this.loginEmail;
     this.loginPassword;
     this.actionCode;
 
-    this.init = function(config){
+    this.init = function (config) {
         this.app = firebase.initializeApp(config);
+        var self = this;
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                self.user = user;
+                self.onLogin();
+            }
+        });
     }
+
     this.createUser = function (email, password, form) {
         console.log(email + "  " + password);
         var self = this;
-        if (this.usernameUnique) {
-            this.doRegister = false;
-            firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
-                self.onCreateUserSuccess(user)
-            }, function (error) {
-                self.onCreateUserError(error)
-            });
-        } else {
-            this.doRegister = true;
-            this.loginEmail = email;
-            this.loginPassword = password;
-        }
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
+            self.onCreateUserSuccess(user)
+        }, function (error) {
+            self.onCreateUserError(error)
+        });
     }
     this.onCreateUserSuccess = function (user) {
         console.log("YAY YOU SIGNED IN!!!!")
         console.log(user);
         $('form').removeClass('loading');
-        $('#uniquemiscerror').addClass('hidden');
+        $(".ui.negative.message").addClass('hidden');
     }
     this.onCreateUserError = function (error) {
         $('form').removeClass('loading');
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.log("ERROR!!!!");
-        console.log("code" + errorCode + "  message:" + errorMessage);
-
-        $('#uniquemiscerror').removeClass('hidden');
-        $('#uniquemiscerror > p').text(errorMessage)
-        //field.addClass('error');
-
+        $(".ui.negative.message").removeClass('hidden');
+        $(".ui.negative.message > p").text(error.message)
     }
-    this.doesUserNameExist = function (name, field) {
-
-        console.log(name);
-        this.usernameUnique = false;
+    this.checkUserData = function (data) {
         var self = this;
-        var usernameRef = firebase.database().ref('/Users').orderByChild('username').equalTo(name);
+        var usernameRef = firebase.database().ref('/Usernames').orderByKey().equalTo(data.username);
         usernameRef.once('value').then(snapshot => {
             var username = snapshot.val();
-            field.removeClass('loading');
-            field.removeClass('error');
-            $('#uniqueusernameerror').addClass('hidden');
+            $(".ui.negative.message").addClass('hidden');
             if (!username) {
                 self.usernameUnique = true;
-                if (self.doRegister) {
-                    self.createUser(self.loginEmail, self.loginPassword);
-                }
-                field.removeClass('error');
+                self.username = name;
+                self.storeUserData(data);
             } else {
-                field.addClass('error');
-                $('#uniqueusernameerror').removeClass('hidden');
+                $('form').removeClass('loading');
+                $(".ui.negative.message").removeClass('hidden');
+                $(".ui.negative.message > p").text("Someone else already picked that");
             }
         })
-
+    }
+    this.storeUserData = function (data) {
+        console.log(this.app.auth());
+        var self = this;
+        var userRef = firebase.database().ref('/Users/' + this.app.auth().currentUser.uid);
+        userRef.set(data);
+        userRef.once('value').then(function (snapshot) {
+            var usernameRef = firebase.database().ref('/Usernames/' + data.username);
+            usernameRef.set(self.app.auth().currentUser.uid);
+            usernameRef.once('value').then(function (snapshot) {
+                console.log("username successfully stored!!");
+            }, function(error) {
+                console.log("ERROR!!");
+                console.log(error.message);
+            });
+        }, function (error){
+            console.log("ERROR!!");
+            console.log(error.message);
+        });
+    }
+    this.onLogin = function () {
+        var self = this;
+        console.log(this.app.auth().currentUser.uid+"  MY UID");
+        var userRef = firebase.database().ref('/Users/' + this.app.auth().currentUser.uid);
+        userRef.once('value').then(function (snapshot) {
+            if(snapshot.val()){
+                showSignout();
+            }else{
+                showUserData();
+            }
+        }, function (error) {
+            console.log(error.message);
+            $(".ui.negative.message").removeClass('hidden');
+            $(".ui.negative.message > p").text(error.message);
+            self.signout();
+        });
     }
     this.login = function (email, password) {
-        console.log("logging in with email:" + email + " and password:" + password);
-
         var self = this;
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
@@ -79,12 +101,22 @@ function FireBaseManager() {
             self.onLoginError(error);
         });
     }
+    this.onLoginComplete = function () {
+        console.log("USER LOGGED IN!");
+        showSignout();
+        console.log(this.app.auth().currentUser);
+        $(".ui.positive.message").removeClass('hidden');
+        $(".ui.positive.message > p").text("Logged in as " + this.app.auth().currentUser);
+    }
     this.signout = function () {
         firebase.auth().signOut().then(function () {
             console.log("signed out");
+            showLogin();
         }, function (error) {
             // An error happened.
             console.log("signout error" + error.message);
+            $(".ui.negative.message").removeClass('hidden');
+            $(".ui.negative.message > p").text(error.message);
         });
     }
     this.onLoginSucccess = function (user) {
@@ -98,7 +130,7 @@ function FireBaseManager() {
         $('form').removeClass('loading');
         console.log($(".ui.negative.message:visible"));
         $(".ui.negative.message").removeClass('hidden');
-        $(".ui.negative.message:visible > p").text(error.message);
+        $(".ui.negative.message > p").text(error.message);
         console.log("ERROR" + error.message);
     }
     this.requestResetPassword = function (email) {
@@ -117,37 +149,39 @@ function FireBaseManager() {
             $(".ui.negative.message:visible > p").text(error.message);
         });
     }
-    this.checkURLParameters = function(){
+    this.checkURLParameters = function () {
         var urlParams = this.getUrlParams();
         var mode = urlParams.mode;
         this.actionCode = urlParams.oobCode;
         var accountEmail;
         // Verify the password reset code is valid.
-        if(!mode) showLogin();
-        else if(mode == "resetPassword"){
+        if (!mode) showLogin();
+        else if (mode == "resetPassword") {
             showReset();
             this.app.auth().verifyPasswordResetCode(this.actionCode).then(function (email) {
                 var accountEmail = email;
                 console.log(accountEmail);
-                console.log("check:"+ $(".ui.message:visible > p"));
-                $(".ui.message:visible>p").text("Resetting password for "+accountEmail);
+                console.log("check:" + $(".ui.message:visible > p"));
+                $(".ui.message:visible>p").text("Resetting password for " + accountEmail);
             }).catch(function (error) {
-                $(".ui.negative.message:visible").removeClass('hidden');
-                $(".ui.negative.message:visible > p").text(error.message);
+                console.log($(".ui.negative.message:visible"))
+                $(".ui.negative.message").removeClass('hidden');
+                $(".ui.negative.message > p").text(error.message);
             });
         }
     }
     this.resetPassword = function (newpassword) {
         $('form').addClass('loading');
-        auth.confirmPasswordReset(this.actioncode, newpassword).then(function (resp) {
+        console.log(this.actionCode)
+        this.app.auth().confirmPasswordReset(this.actionCode, newpassword).then(function (resp) {
             // navigate back to login url.split('?')[0]
             $('form').removeClass('loading');
             console.log("BIG SUCCES, password reset succesful");
-            $(".ui.positive.message:visible").removeClass('hidden');
+            $(".ui.positive.message").removeClass('hidden');
         }).catch(function (error) {
             $('form').removeClass('loading');
-            $(".ui.negative.message:visible").removeClass('hidden');
-            $(".ui.negative.message:visible > p").text(error.message);
+            $(".ui.negative.message").removeClass('hidden');
+            $(".ui.negative.message > p").text(error.message);
         });
     }
 
