8a580dfea7eddcaee4a3017e27e80c7ee6df4304
Erik Sombroek <eriksombroek@gmail.com>
2021-02-19 09:11:16 +0100

    Fixed forcefield texture update

--- src/b2Editor/classes/TiledMesh.js
+++ src/b2Editor/classes/TiledMesh.js
@@ -18,29 +18,30 @@ export class TiledMesh extends PIXI.SimpleMesh {
 		this.targetSprite = null;
 		this.cachedSpriteRotation = 0;
 		this.verticesClone = Float32Array.from(vertices);
-		this.dirty = 0;
 		this.fixedTextureRotationOffset = Math.PI / 2;
 		this.gradientMode = false;
 	}
 
 	updateMeshVerticeRotation (force = false) {
-		console.log("UPDATE!", this);
+
 		const targetSprite = this.targetSprite;
 		const tex = this.tex;
 
 		if(this.cachedSpriteRotation !== targetSprite.rotation || force){
-	
-			for (let i = 0; i < this.verticesClone.length; i += 2){
-				let x = this.verticesClone[i];
-				let y = this.verticesClone[i+1];
+
+			const vertices = Float32Array.from(this.verticesClone);
+
+			for (let i = 0; i < vertices.length; i += 2){
+				let x = vertices[i];
+				let y = vertices[i+1];
 				let l = Math.sqrt(x*x+y*y);
 				let a = Math.atan2(y, x);
 
 				a += targetSprite.rotation;
 				a += this.fixedTextureRotationOffset;
 
-				this.vertices[i] = l * Math.cos(a);
-				this.vertices[i+1] = l * Math.sin(a);
+				vertices[i] = l * Math.cos(a);
+				vertices[i+1] = l * Math.sin(a);
 			}
 
 			let minX = Number.POSITIVE_INFINITY;
@@ -48,11 +49,11 @@ export class TiledMesh extends PIXI.SimpleMesh {
 			let minY = Number.POSITIVE_INFINITY;
 			let maxY = -Number.POSITIVE_INFINITY;
 
-			const uvs = new Float32Array(this.vertices.length);
+			const uvs = new Float32Array(vertices.length);
 
-			for (let i = 0; i < this.vertices.length; i+=2) {
-				uvs[i] = this.vertices[i] * 2.0 / tex.width + 0.5;
-				uvs[i + 1] = this.vertices[i + 1] * 2.0 / tex.width + 0.5;
+			for (let i = 0; i < vertices.length; i+=2) {
+				uvs[i] = vertices[i] * 2.0 / tex.width + 0.5;
+				uvs[i + 1] = vertices[i + 1] * 2.0 / tex.width + 0.5;
 
 				minX = Math.min(uvs[i + 0], minX);
 				maxX = Math.max(uvs[i + 0], maxX);
@@ -67,10 +68,11 @@ export class TiledMesh extends PIXI.SimpleMesh {
 				}
 			}
 
-			this.uvs = uvs;
+			this.verticesBuffer.update(vertices);
+			this.uvBuffer.update(uvs);
+
 			this.rotation = -targetSprite.rotation-this.fixedTextureRotationOffset;
 			this.cachedSpriteRotation = targetSprite.rotation
-			this.dirty++;
 		}
 	}
 }
