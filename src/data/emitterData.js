4563293097ef38310d3af2c796fe9432faecb442
Erik Sombroek <eriksombroek@gmail.com>
2021-05-21 11:38:23 +0200

    Added exploding particles to the breakables

--- src/data/emitterData.js
+++ src/data/emitterData.js
@@ -541,3 +541,57 @@ export const confetti = {
 	"ease":easing.easeInOutQuad,
 	"emit":false
 }
+
+export const splash = {
+	"alpha": {
+		"start": 1,
+		"end": 0
+	},
+	"scale": {
+		"start": 0.1,
+		"end": 0.59,
+		"minimumScaleMultiplier": 2
+	},
+	"color": {
+		"start": "#ffffff",
+		"end": "#ffffff"
+	},
+	"speed": {
+		"start": 30,
+		"end": 0,
+		"minimumSpeedMultiplier": 0.001
+	},
+	"acceleration": {
+		"x": 0,
+		"y": 0
+	},
+	"maxSpeed": 0,
+	"startRotation": {
+		"min": 0,
+		"max": 360
+	},
+	"noRotation": false,
+	"rotationSpeed": {
+		"min": 0,
+		"max": 0
+	},
+	"lifetime": {
+		"min": 0.2,
+		"max": 0.4
+	},
+	"blendMode": "normal",
+	"frequency": 0.005,
+	"emitterLifetime": 0.1,
+	"maxParticles": 1000,
+	"pos": {
+		"x": 0,
+		"y": 0
+	},
+	"addAtBack": false,
+	"spawnType": "circle",
+	"spawnCircle": {
+		"x": 0,
+		"y": 0,
+		"r": 20
+	}
+}
--- src/prefabs/misc/SoftBreakable.js
+++ src/prefabs/misc/SoftBreakable.js
@@ -4,17 +4,22 @@ import {
     game
 } from "../../Game";
 
+import * as emitterManager from '../../utils/EmitterManager'
+import * as PhysicsParticleEmitter from '../../utils/PhysicsParticleEmitter';
 
 export class SoftBreakable extends PrefabManager.basePrefab {
     constructor(target) {
         super(target);
 
-		this.life = 100;
-		this.splashColor = '#FFFFFF';
-		this.splashTexture = 'Splash';
-		this.partsColors = ['#FFFFFF'];
+		this.breakingForce = 40;
+		this.splashColors = ['#FFFFFF'];
+		this.emitterType = 'splash';
+		this.partsColors = [0xFFFFFF];
 		this.partsType = ['Pumpkin2', 3];
 		this.partsQuantity = 6;
+		this.partsSize = 5;
+		this.partsForce = 10;
+		this.doBreak = false;
 
 	}
 
@@ -23,28 +28,28 @@ export class SoftBreakable extends PrefabManager.basePrefab {
         this.base = this.lookupObject['base'];
 	}
 
-	dealDamage(damage){
-		this.life -= damage;
+	break(){
+		emitterManager.playOnceEmitter(this.emitterType, null, this.base.GetPosition(), 0, this.splashColors);
 
-		console.log(this.life);
-		if(this.life<= 0){
-			this.destroy();
+		const parts = [];
+		for(let i = 0; i <this.partsType[1]; i++){
+			parts.push(`${this.partsType[0]}_${i+1}`);
 		}
+
+		PhysicsParticleEmitter.emit(parts, this.base.GetPosition(), this.partsQuantity, this.partsSize, this.partsForce, false, this.partsColors);
+		this.destroy();
 	}
 
-	destroy(){
-		super.destroy();
+	update(){
+		if(this.doBreak){
+			this.break();
+		}
 	}
 
 	initContactListener() {
         super.initContactListener();
         const self = this;
         this.contactListener.PostSolve = function (contact, impulse) {
-            const bodies = [contact.GetFixtureA().GetBody(), contact.GetFixtureB().GetBody()];
-
-            const baseBody = (bodies[0] == self.base) ? bodies[0] : bodies[1];
-            const otherBody = (bodies[0] == self.base) ? bodies[1] : bodies[0];
-
 			let force = 0;
 			for (let j = 0; j < impulse.get_count(); j++){
 				if (impulse.get_normalImpulses(j) > force){
@@ -52,19 +57,9 @@ export class SoftBreakable extends PrefabManager.basePrefab {
 				}
 			}
 
-			const velocityA = contact.GetFixtureA().GetBody().GetLinearVelocity().Length();
-			const velocityB = contact.GetFixtureB().GetBody().GetLinearVelocity().Length();
-			let impactAngle = (velocityA > velocityB) ? Math.atan2(contact.GetFixtureA().GetBody().GetLinearVelocity().get_y(), contact.GetFixtureA().GetBody().GetLinearVelocity().get_x()) : Math.atan2(contact.GetFixtureB().GetBody().GetLinearVelocity().get_y(), contact.GetFixtureB().GetBody().GetLinearVelocity().get_x());
-			impactAngle *= game.editor.RAD2DEG + 180;
-			const velocitySum = velocityA + velocityB;
-
-			console.log("Velocity sum:", velocitySum);
-
 			const skipBecauseToLight = contact.GetFixtureA().GetDensity() === 0.001 || contact.GetFixtureB().GetDensity() === 0.001;
-			if (velocitySum > 6.0 && !skipBecauseToLight) {
-
-				const slidingDamageScalar = 50;
-				self.dealDamage(force/slidingDamageScalar);
+			if (force > self.breakingForce && !skipBecauseToLight) {
+				self.doBreak = true;
 			}
 		}
     }
