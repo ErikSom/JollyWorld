70eb27eb24832a398f89679d82526cc348cdf2c7
Erik Sombroek <eriksombroek@gmail.com>
2021-05-21 06:38:56 +0200

    WIP breakables

--- src/prefabs/misc/Cola.js
+++ src/prefabs/misc/Cola.js
@@ -1,15 +1,15 @@
+import { SuperEmitter } from '../../utils/SuperEmitter';
 import * as PrefabManager from '../PrefabManager';
-import {
-    game
-} from "../../Game";
 import {SoftBreakable} from './SoftBreakable'
 
 class Cola extends SoftBreakable {
-    constructor(target) {
-	}
+    init(){
+        super.init();
+        this.life = 120;
+    }
 }
 
-PrefabManager.prefabLibrary.CannonBall = {
+PrefabManager.prefabLibrary.Cola = {
     json: '{"objects":[[0,0.0024,-0.0092,0,"","",0,["#999999"],["#000"],[1],false,true,[[{"x":0.3228,"y":-1.19},{"x":0.3228,"y":1.19},{"x":-0.3228,"y":1.19},{"x":-0.3228,"y":-1.19}]],[1],[0],[0],"",[0],false,false,false,[0.5],[0.2],false,true,false],[1,-0.072,0.2759,0,"","",1,"Cola instance 10000",0,0.5703,-1.826,0,false,"#FFFFFF",1,1,1,0,0,0,true]]}',
     class: Cola,
     library: PrefabManager.LIBRARY_MISC
--- src/prefabs/misc/SoftBreakable.js
+++ src/prefabs/misc/SoftBreakable.js
@@ -1,12 +1,71 @@
 import * as PrefabManager from '../PrefabManager';
 
+import {
+    game
+} from "../../Game";
+
+
 export class SoftBreakable extends PrefabManager.basePrefab {
     constructor(target) {
+        super(target);
+
+		this.life = 100;
+		this.splashColor = '#FFFFFF';
+		this.splashTexture = 'Splash';
+		this.partsColors = ['#FFFFFF'];
+		this.partsType = ['Pumpkin2', 3];
+		this.partsQuantity = 6;
+
 	}
 
-    destroy(){
+	init(){
+		super.init();
+        this.base = this.lookupObject['base'];
+	}
+
+	dealDamage(damage){
+		this.life -= damage;
+
+		console.log(this.life);
+		if(this.life<= 0){
+			this.destroy();
+		}
+	}
+
+	destroy(){
+		super.destroy();
+	}
+
+	initContactListener() {
+        super.initContactListener();
+        const self = this;
+        this.contactListener.PostSolve = function (contact, impulse) {
+            const bodies = [contact.GetFixtureA().GetBody(), contact.GetFixtureB().GetBody()];
+
+            const baseBody = (bodies[0] == self.base) ? bodies[0] : bodies[1];
+            const otherBody = (bodies[0] == self.base) ? bodies[1] : bodies[0];
+
+			let force = 0;
+			for (let j = 0; j < impulse.get_count(); j++){
+				if (impulse.get_normalImpulses(j) > force){
+					force = impulse.get_normalImpulses(j);
+				}
+			}
+
+			const velocityA = contact.GetFixtureA().GetBody().GetLinearVelocity().Length();
+			const velocityB = contact.GetFixtureB().GetBody().GetLinearVelocity().Length();
+			let impactAngle = (velocityA > velocityB) ? Math.atan2(contact.GetFixtureA().GetBody().GetLinearVelocity().get_y(), contact.GetFixtureA().GetBody().GetLinearVelocity().get_x()) : Math.atan2(contact.GetFixtureB().GetBody().GetLinearVelocity().get_y(), contact.GetFixtureB().GetBody().GetLinearVelocity().get_x());
+			impactAngle *= game.editor.RAD2DEG + 180;
+			const velocitySum = velocityA + velocityB;
+
+			console.log("Velocity sum:", velocitySum);
+
+			const skipBecauseToLight = contact.GetFixtureA().GetDensity() === 0.001 || contact.GetFixtureB().GetDensity() === 0.001;
+			if (velocitySum > 6.0 && !skipBecauseToLight) {
+
+				const slidingDamageScalar = 50;
+				self.dealDamage(force/slidingDamageScalar);
+			}
+		}
     }
-
-	update(){
-	}
 }
