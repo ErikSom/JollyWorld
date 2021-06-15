f8a7d024986bcf7ad8eab3230637528c49b92e8a
Erik Sombroek <eriksombroek@gmail.com>
2021-06-15 10:21:20 +0200

    Fixed some mod shit again

---
+++ asset_src/jollymod copy/_README.txt
@@ -0,0 +1,10 @@
+How to mod JollyWorld textures:
+
+1 ) Change all the textures you would like to change
+     - don't change the image width & height
+     - eyes and limbs are positioned in fixed places, keep that in mind
+     - (Optional) remove folders you didn't mod for faster installation
+2 ) Compress your mod to a ZIP file and give it a good name (must be zip!)
+3 ) Upload the mod to https://jollyworld.app/mod
+
+* you know what is and what isn't allowed, be mindful when spreading mods!
--- src/utils/ModManager.js
+++ src/utils/ModManager.js
@@ -3,11 +3,9 @@ import {
 } from "../Game";
 
 import * as idb from 'idb-keyval';
+import { Settings } from "../Settings";
 
 const folderName = 'jollymod';
-
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d', {alpha:true});
 
 export const init = ()=>{
 	idb.keys().then(keys => {
@@ -15,6 +13,8 @@ export const init = ()=>{
 
 		const characterMods = [];
 		const vehicleMods = [];
+		const goreMods = [];
+		const textureMods = [];
 
 		if(keys.find( key => key.startsWith(folderName))){
 
@@ -25,6 +25,12 @@ export const init = ()=>{
 				if(key.indexOf(`${folderName}/vehicles`) === 0){
 					vehicleMods.push(key);
 				}
+				if(key.indexOf(`${folderName}/gore`) === 0){
+					goreMods.push(key);
+				}
+				if(key.indexOf(`${folderName}/textures`) === 0){
+					textureMods.push(key);
+				}
 			})
 
 			if(characterMods.length > 0){
@@ -33,6 +39,14 @@ export const init = ()=>{
 
 			if(vehicleMods.length > 0){
 				modVehicles(vehicleMods);
+			}
+
+			if(goreMods.length > 0){
+				modGore(goreMods);
+			}
+
+			if(textureMods.length > 0){
+				modTexture(textureMods);
 			}
 
 			console.log(game.app, characterMods.length, vehicleMods.length);
@@ -55,18 +69,36 @@ const modVehicles = vehicleMods => {
 	modAtlas('Vehicles_1.json_image', 'Vehicles_1.json', vehicleMods);
 }
 
+const modGore = goreMods => {
+	modAtlas('Characters_Gore.json_image', 'Characters_Gore.json', goreMods);
+}
+
+const modTexture = textureMods => {
+	textureMods.forEach(key => {
+		console.log(key);
+		const baseName = key.split('.')[0];
+		const number = parseInt(baseName.substr(baseName.length-4, 4), 10);
+		const realTextureName = Settings.textureNames[number];
+		modAtlas(realTextureName, '', [key]);
+	});
+}
+
 const modAtlas = (textureName, jsonName, textures) => {
 	// firsts lets grab our atlas base texture;
 
 	const targetBaseTextureCache = PIXI.utils.BaseTextureCache[textureName];
 	const resource = targetBaseTextureCache.resource;
 
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d', {alpha:true});
+
 	canvas.width = resource.width;
 	canvas.height = resource.height;
 
 	ctx.drawImage(resource.source, 0, 0);
 
-	const atlas = game.app.loader.resources[jsonName].data.frames;
+	let atlas = null;
+	if(jsonName) atlas = game.app.loader.resources[jsonName].data.frames;
 
 	const expectedMods = textures.length;
 	let finishedMods = 0;
@@ -76,23 +108,42 @@ const modAtlas = (textureName, jsonName, textures) => {
 		const path = mod.split('/');
 		const textureName = path[path.length-1].split('.')[0];
 
-		const {x, y, w, h} = atlas[textureName].frame;
+		let frame = null;
+
+		if(!jsonName){
+			frame = {x:0, y:0, w:targetBaseTextureCache.resource.width, h:targetBaseTextureCache.resource.height}
+		}else{
+			frame = atlas[textureName].frame;
+		}
+
+		const {x, y, w, h} = frame;
 
 		idb.get(mod).then(blob => {
 			const url = URL.createObjectURL(blob);
 			const image = new Image();
-			document.body.appendChild(image);
 			image.src = url;
 			image.onload = () => {
 				ctx.clearRect(x, y, w, h);
 				ctx.drawImage(image, x, y, w, h);
 				finishedMods++;
 				if(finishedMods === expectedMods){
-					const canvasResource = new PIXI.CanvasResource(canvas);
-					targetBaseTextureCache.resource.dispose();
-					delete targetBaseTextureCache.resource;
-					targetBaseTextureCache.setResource(canvasResource);
-					targetBaseTextureCache.dirtyId++;
+					if(jsonName){
+						const canvasResource = new PIXI.CanvasResource(canvas);
+						targetBaseTextureCache.resource.dispose();
+						delete targetBaseTextureCache.resource;
+						targetBaseTextureCache.setResource(canvasResource);
+						targetBaseTextureCache.dirtyId++;
+					}else{
+						const imageResource = new Image();
+						imageResource.src = canvas.toDataURL();
+						imageResource.onload = ()=>{
+							const imageResourceClass = new PIXI.ImageResource(imageResource);
+							targetBaseTextureCache.resource.dispose();
+							delete targetBaseTextureCache.resource;
+							targetBaseTextureCache.setResource(imageResourceClass);
+							targetBaseTextureCache.dirtyId++;
+						}
+					}
 				}
 			}
 		}).catch(err => {
