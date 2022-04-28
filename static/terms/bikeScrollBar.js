6a2ccf14a2d66952226e861842890a12a4aa5b56
Warze <40837031+WarzeEgg@users.noreply.github.com>
2022-04-28 14:52:43 +0200

    All the redesign changes

---
+++ static/terms/bikeScrollBar.js
@@ -0,0 +1,19 @@
+const bicycle = document.querySelector('.bicycle');
+const scroll = document.querySelector('.scroll')
+var old_scroll_top = 0;
+document.body.onresize = updateBicycle;
+scroll.onscroll = updateBicycle;
+function updateBicycle() {
+	if (event && event.type === 'scroll') {
+		if (old_scroll_top > scroll.scrollTop) {
+			bicycle.style.transform = "scaleY(-1)";
+		} else {
+			bicycle.style.transform = "scaleY(1)";
+		}
+	}
+	old_scroll_top = scroll.scrollTop;
+	var offset = (window.innerHeight / (scroll.scrollHeight / window.innerHeight) - 82) / 2
+	bicycle.style.top = scroll.scrollTop / scroll.scrollHeight * window.innerHeight + offset + 'px';
+}
+document.body.onload = updateBicycle;
+updateBicycle()
